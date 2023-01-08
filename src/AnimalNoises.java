17fe47d7d1eda69b7623cabf16cc6244914cc412
lscha <lscha@DESKTOP-6B90PBC>
2023-01-08 17:41:43 -0600

    noises

---
+++ src/AnimalNoises.java
@@ -0,0 +1,13 @@
+import model.Example
+
+public class AnimalNoises {
+	
+	// add your animal class to the model package
+	// only push this file and the animal class you created
+
+	public static void main(String[] args) {
+		Example example = new Example();
+		System.out.println(example.makeNoise());
+	}
+
+}
