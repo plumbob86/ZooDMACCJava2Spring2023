7ed1c7a450d82cd2569b21508bccf97f8a51227c
EmeraldVoltron <40584963+EmeraldVoltron@users.noreply.github.com>
2023-01-13 21:30:38 -0600

    Added a wolf to the zoo

---
+++ AnimalNoises.java
@@ -0,0 +1,30 @@
+//added semicolon to the end of import (Andrew Steele 01/10/2023)
+import model.Example;
+import model.Platypus;
+import model.Wolf;
+import model.Dog;
+import model.Cat;
+public class AnimalNoises {
+	
+	// add your animal class to the model package
+	// only push this file and the animal class you created
+
+	public static void main(String[] args) {
+		Example example = new Example();
+		System.out.println(example.makeNoise());
+		//created a new instance of the object and output the sound it makes
+		Platypus frank = new Platypus();
+		System.out.println(frank.makeNoise());
+		Dog Titan = new Dog();
+		System.out.println(Titan.makeNoise());
+		
+		Cat jordan = new Cat("Jordan",6,true);
+		System.out.println(jordan.getName() + " Age: " + jordan.getAge() + " Is Loved: " + jordan.getIsLoved());
+		System.out.println(jordan.getName() + ": " + jordan.speak());
+		
+		//Abby Boggs 1.13.23 - added wolf instance and noise
+		Wolf redd = new Wolf("Redd", "forest", "red");
+		System.out.println(redd.makeNoise());
+	}
+
+}
