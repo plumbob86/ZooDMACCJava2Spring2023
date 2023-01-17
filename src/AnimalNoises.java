a3d37068334d6839fdddf97a8f97af6fc943b61a
VCT0RR <abhishekryan144@gmail.com>
2023-01-17 15:06:00 -0600

    added new animal class and modified AnimalNoises.java

--- src/AnimalNoises.java
+++ src/AnimalNoises.java
@@ -1,9 +1,9 @@
 //added semicolon to the end of import (Andrew Steele 01/10/2023)
 import model.Example;
 import model.Platypus;
-
+import model.Wolf;
 import model.Dog;
-import model.Example
+import model.Example;
 import model.Example;
 import model.Cat;
 public class AnimalNoises {
@@ -23,6 +23,10 @@ public class AnimalNoises {
 		Cat jordan = new Cat("Jordan",6,true);
 		System.out.println(jordan.getName() + " Age: " + jordan.getAge() + " Is Loved: " + jordan.getIsLoved());
 		System.out.println(jordan.getName() + ": " + jordan.speak());
+		
+		Wolf Seth = new Wolf("Seth", "Blue", 34);
+		System.out.println("Name: " + Seth.getName() + "; Eye Color: " + Seth.getEyeColor() + "; Size/Height(in inches): " + Seth.getSize());
+		System.out.println(Seth.toString());
+		System.out.println(Seth.speak());
 	}
-
 }
